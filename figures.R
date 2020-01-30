bb2db934f2e73b3cd940cb2bbd20614379b38974
Claudio Fronterre <cla.f90@gmail.com>
2020-01-30 13:39:56 +0000

    added code for map with africa in background

--- figures.R
+++ figures.R
@@ -13,6 +13,7 @@ malaria_sp <- malaria %>%
 
 # load the administrative boundaries of Tanzania
 tz <- st_read("data/geodata/gadm36_TZA.gpkg", layer = "gadm36_TZA_0")
+africa <- st_read("data/geodata/africa.gpkg")
 
 # OBSERVED PREVALENCE MAP ------------------------------------------------------
 
@@ -42,6 +43,7 @@ create_labels <- function(x) {
 # Generate labels for legend
 labs <- create_labels(brks)
 
+# Generate the maps
 tm_shape(tz, is.master = T) +
   tm_polygons(col = "white", border.col = "black") +
 tm_shape(malaria_sp) +
@@ -64,3 +66,28 @@ tm_layout(design.mode = F, legend.bg.color = "white",
 
 # Save the map
 tmap_save(filename = "figs/figure1_prevmap.pdf", width = 7, height = 6.25)  
+
+
+# Version with Africa in background --------------------------------------------
+tm_shape(africa, filter = africa$COUNTRY == "Tanzania", bbox = tz,
+         ext = 1.3) +
+  tm_polygons(col = "white", colorNULL = "grey90", border.col = "black", ) +
+tm_shape(malaria_sp) +
+  tm_symbols(col = "prev100", shape = 21, border.col = "black",
+             palette = pal, size = .1, border.lwd = .7,
+             style = "fixed", breaks = brks, legend.col.show = F) +
+  tm_add_legend(type = "symbol",
+                labels = labs,
+                shape = 21, col = pal,
+                border.col = "black",
+                size = .6, alpha = 1, 
+                title = "P.falciparum\nprevalence (%)") +
+tm_compass(position = c("left", "top")) +
+tm_scale_bar(position = c("right", "BOTTOM"), text.size = 0.7) +
+tm_layout(design.mode = F, legend.bg.color = "white",  
+          legend.position = c("left", "bottom"), legend.frame = T,
+          bg.color = "lightblue",
+          outer.margins = 0, asp = 0, panel.show = T, 
+          panel.labels = "TITLE") 
+
+tmap_save(filename = "figs/figure1_prevmap_africa.pdf", width = 6)  
