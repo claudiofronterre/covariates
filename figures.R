3aa8bad3fa64fd4e63eb98f7388e90710c7f84f4
Claudio Fronterre <cla.f90@gmail.com>
2020-02-03 13:36:07 +0000

    add code and figure with country background

--- figures.R
+++ figures.R
@@ -1,10 +1,10 @@
 # LOAD ALL PACKAGES NEEDED FOR THE ANALYSIS ------------------------------------
 if (!require("pacman")) install.packages("pacman")
-pkgs = c("sf", "tmap")
+pkgs = c("sf", "tmap", "tmaptools", "readr")
 pacman::p_load(pkgs, character.only = T)
 
 # LOAD THE DATASETS ------------------------------------------------------------
-malaria <- readr::read_csv("data/TZ_2015.csv")
+malaria <- read_csv("data/TZ_2015.csv")
 
 # convert the data frame to a sf object (spatial object)
 # 4326 is the EPSG code for lat long WGS84 
@@ -24,7 +24,7 @@ malaria_sp$prev100 <- (malaria_sp$Pf / malaria_sp$Ex) * 100
 malaria_sp <- malaria_sp[order(malaria_sp$prev100), ]
 
 # Define color palette for plotting
-pal <- tmaptools::get_brewer_pal("-RdYlBu", n = 8, contrast = c(0, 1), plot = F)
+pal <- get_brewer_pal("-RdYlBu", n = 8, contrast = c(0, 1), plot = F)
 
 # Define breaks 
 brks <- round(c(0, quantile(malaria_sp$prev100[malaria_sp$prev100 > 0],
@@ -70,7 +70,7 @@ tmap_save(filename = "figs/figure1_prevmap.pdf", width = 7, height = 6.25)
 # Version with Africa in background --------------------------------------------
 tm_shape(africa, filter = africa$COUNTRY == "Tanzania", bbox = tz,
          ext = 1.3) +
-  tm_polygons(col = "white", colorNULL = "grey90", border.col = "black", ) +
+  tm_polygons(col = "white", colorNULL = "grey90", border.col = "black") +
 tm_shape(malaria_sp) +
   tm_symbols(col = "prev100", shape = 21, border.col = "black",
              palette = pal, size = .1, border.lwd = .7,
@@ -90,4 +90,29 @@ tm_layout(design.mode = F, legend.bg.color = "white",
 
 tmap_save(filename = "figs/figure1_prevmap_africa.pdf", width = 6)  
 
-# Version with COUNTRY background ----------------------------------------------
+# VERSION WITH COUNTRY BACKGROUND ----------------------------------------------
+osm_tz <- read_cosm(tz)
+
+tm_shape(osm_tz) +
+  tm_rgb() +
+tm_shape(africa, filter = africa$COUNTRY != "Tanzania", bbox = tz,
+         ext = 1.3, is.master = T) +
+  tm_polygons(col = "grey90", colorNULL = NULL, border.col = "black") +
+tm_shape(malaria_sp) +
+  tm_symbols(col = "prev100", shape = 21, border.col = "black",
+             palette = pal, size = .1, border.lwd = .7,
+             style = "fixed", breaks = brks, legend.col.show = F) +
+  tm_add_legend(type = "symbol",
+                labels = labs,
+                shape = 21, col = pal,
+                border.col = "black",
+                size = .6, alpha = 1, 
+                title = "P.falciparum\nprevalence (%)") +
+  tm_compass(position = c("left", "top")) +
+  tm_scale_bar(position = c("right", "BOTTOM"), text.size = 0.7) +
+  tm_layout(design.mode = F, legend.bg.color = "white",  
+            legend.position = c("left", "bottom"), legend.frame = T,
+            bg.color = "lightblue",
+            outer.margins = 0, asp = 0) 
+# save the map
+tmap_save(filename = "figs/figure1_prevmap_background.pdf", width = 6)  
